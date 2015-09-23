65485697d8313dba91e39016dbac186bacd3ab52
Manuel Klimek <klimek@klimek-macbookpro2.roam.corp.google.com>
2015-09-23 09:28:16 -0700

    example

--- example.cc
+++ example.cc
@@ -1,9 +1,17 @@
 #include <vector>
 
-int sum(const std::vector<int> &v) {
+int sum1(const std::vector<int> &v) {
   int result = 0;
   for (size_t i = 0; i < v.size(); ++i) {
     result += v[i];
+  }
+}
+
+int sum2(const std::vector<int> &v) {
+  int result = 0;
+  for (std::vector<int>::const_iterator i = v.begin(), e = v.end(); i != e;
+       ++i) {
+    result += *i;
   }
   return result;
 }
